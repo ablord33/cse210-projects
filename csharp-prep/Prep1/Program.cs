181e57462c4ba747214ad0d93ad1f5f19655d236
ablord33 <abl23001@byui.edu>
2023-04-22 13:50:50 +0100

    Edited the code and added first and last names.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("what is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("what is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"your name is {last}, {first} {last}");        
+
     }
+    
+
 }
